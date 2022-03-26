cbb680233dcfd5123d89b6fc2d3ea305f175e295
hajinopro <hajinopro@gmail.com>
2022-03-26 18:42:43 +0900

    Chapter 13 completed

---
+++ Cards/Model/CardModal.swift
@@ -0,0 +1,12 @@
+//
+//  CardModal.swift
+//  Cards
+//
+//  Created by 하진호 on 2022/03/26.
+//
+
+import SwiftUI
+
+enum CardModal {
+    case photoPicker, framePicker, stickerPicker, textPicker
+}
---
+++ Cards/Model/ViewState.swift
@@ -0,0 +1,12 @@
+//
+//  ViewState.swift
+//  Cards
+//
+//  Created by 하진호 on 2022/03/26.
+//
+
+import SwiftUI
+
+class ViewState: ObservableObject {
+    @Published var showAllCards = true
+}
